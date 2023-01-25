9a867b06fc9a7b2ceeaff37167f627d81a565070
rajaa maati <rajaamaati1998@gmail.com>
2023-01-25 19:37:24 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+
+let a = 'La révolution ne sera pas télévisée.';
+count = function(a) {
+  
+  console.log(a); 
+  n = a.length;
+  console.log(n); 
+  var c = 0;
+  for (i = 0; i < n; i++) {
+    if ((a[i] == "a") || (a[i] == "e") || (a[i] == "i") || (a[i] == "o") || (a[i] == "u")) {
+      console.log(a[i]);
+      c += 1;
+    }
+  }
+
+  document.getElementById("p").innerText = c;
+}
+    
+    console.log(a.split());
+    while (a =! '.')
+ 
+ console.log(a) ;
+   // ---------------------------------------------------------------
+  
+
+
+   
+
+
+
+
+
